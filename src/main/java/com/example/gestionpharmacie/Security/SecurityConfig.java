464b12b3cfde109a68538c22cb8705fa1ceb2be7
MohamedBelfaquih <lahnin.010@gmail.com>
2024-05-14 21:42:34 +0200

    just add

--- src/main/java/com/example/gestionpharmacie/Security/SecurityConfig.java
+++ src/main/java/com/example/gestionpharmacie/Security/SecurityConfig.java
@@ -1,9 +1,18 @@
 package com.example.gestionpharmacie.Security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -11,6 +20,25 @@ public class SecurityConfig {
 
     @Autowired
     UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService,
+                                                       PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        return new ProviderManager(authenticationProvider);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 
 
 }
--- src/main/java/com/example/gestionpharmacie/Utilisateur/UtilisateurController.java
+++ src/main/java/com/example/gestionpharmacie/Utilisateur/UtilisateurController.java
@@ -1,7 +1,13 @@
 package com.example.gestionpharmacie.Utilisateur;
 
+import com.example.gestionpharmacie.Dto.LoginRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -12,9 +18,15 @@ import java.net.URI;
 @RequestMapping("/utilisateur")
 public class UtilisateurController {
     private final UtilisateurService utilisateurService;
+    private final AuthenticationManager authenticationManager;
+    private final PasswordEncoder passwordEncoder;
     @Autowired
-    public UtilisateurController(UtilisateurService utilisateurService) {
+    public UtilisateurController(UtilisateurService utilisateurService,
+                                  AuthenticationManager authenticationManager,
+                                  PasswordEncoder passwordEncoder) {
         this.utilisateurService = utilisateurService;
+        this.authenticationManager = authenticationManager;
+        this.passwordEncoder = passwordEncoder;
     }
     @GetMapping("/{requestedId}")
     public ResponseEntity<Utilisateur> findById(@PathVariable Long requestedId) {
@@ -27,12 +39,20 @@ public class UtilisateurController {
         }
     }
     @PostMapping
-    public ResponseEntity<Void> Enregistrer(@RequestBody Utilisateur newUtilisateur, UriComponentsBuilder ucb) {
+    public ResponseEntity<Void> enregistrer(@RequestBody Utilisateur newUtilisateur, UriComponentsBuilder ucb) {
+        String hashedPassword = passwordEncoder.encode(newUtilisateur.getPassword());
+        newUtilisateur.setPassword(hashedPassword);
         Utilisateur savedUtilisateur = utilisateurService.addUtilisateur(newUtilisateur);
         URI locationOfMedicament = ucb
                 .path("utilisateur/{id}")
                 .buildAndExpand(savedUtilisateur.getId())
                 .toUri();
         return ResponseEntity.created(locationOfMedicament).build();
+    }
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+        Authentication authenticationRequest = UsernamePasswordAuthenticationToken.unauthenticated(loginRequest.getUsername(), loginRequest.getPassword());
+        Authentication authenticationResponse = this.authenticationManager.authenticate(authenticationRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(authenticationResponse.toString());
     }
 }
